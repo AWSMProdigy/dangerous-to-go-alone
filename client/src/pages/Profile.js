804fe78748d1a1c56e32225a597c09fcd15d7b00
BeauChewning <beauchewning96@gmail.com>
2021-12-01 23:42:20 -0500

    first commit on game list

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
-import ThoughtForm from '../components/ThoughtForm';
-import ThoughtList from '../components/ThoughtList';
-
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
@@ -39,26 +35,48 @@ const Profile = () => {
     <div>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} games.
         </h2>
-
-        <div className="col-12 col-md-10 mb-5">
-          <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-            showTitle={false}
-            showUsername={false}
-          />
-        </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
-            <ThoughtForm />
-          </div>
-        )}
+        <button>Add Games</button>
       </div>
+      <div class="row">
+
+        <div class="col-sm-6 col-md-4">
+            <div class="card border-white">
+                <div class="card-header">Heading 1</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build 
+                           on the card 
+                        title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+            <div class="card border-white">
+                <div class="card-header">Heading 1</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build 
+                           on the card 
+                        title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div><div class="col-sm-6 col-md-4">
+            <div class="card border-white">
+                <div class="card-header">Heading 1</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build 
+                           on the card 
+                        title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+      </div>
+
+
+
+     
+
+
     </div>
   );
 };
