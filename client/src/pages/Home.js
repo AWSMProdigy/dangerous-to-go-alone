508cdcc13a3cb8453dc39df95023b6af3fb2cb25
Leah Davis <ileahd96@gmail.com>
2021-12-02 14:17:57 -0500

    Small text changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
           <img className="img-fluid" src={manGaming} alt="Man staring at a screen with gaming headphones on"></img>
         </div>
       <div className="col-3 my-auto mx-1">
-        <h1 className="text-light mb-3">It's Dangerous To Go Alone...Take A Friend!</h1>
+        <h1 className="text-light mb-3">It's Dangerous To Go Alone... Take A Friend!</h1>
         <p className="text-light">Need some buds to play with or just want to organize the time spent gaming with your friends better? You've come to the right place.</p>
         <p className="text-light">Dangerous To Go Alone helps assist in pairing up friends and randos alike so no one has to play alone!</p>
         <Link to="/signup">
