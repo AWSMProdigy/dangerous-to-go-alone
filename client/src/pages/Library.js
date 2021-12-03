a3c2c4cf74eefd6b8e2a739ed2d4720ad47df2e2
Leah Davis <ileahd96@gmail.com>
2021-12-03 10:48:04 -0500

    Updated game library and homepage design

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import "../../src/library.css";
 import "../../src/styles.css";
 
 
-import battle from "../assets/images/libraryImages/battle.jpg"
-import halo from "../assets/images/libraryImages/halo.jpg"
-import league from "../assets/images/libraryImages/league.jpg"
-import cod from "../assets/images/libraryImages/cod.jpg"
+import battle from "../assets/images/libraryImages/battlefield.jpg"
+import halo from "../assets/images/libraryImages/haloinfinite.jpg"
+import league from "../assets/images/libraryImages/LoL.jpg"
+import cod from "../assets/images/libraryImages/blackops.jpg"
+import forza from "../assets/images/libraryImages/forza.jpg"
+import stardew from "../assets/images/libraryImages/stardew.jpg"
 const Library = () =>{
 return (
-    <div>
+    <div className="container mb-3">
       <div className="flex-row mb-3">
-        <h2 className="col-12 bg-dark text-white p-3 mb-5">
-          Viewing your games
+        <h2 className="col-12 p-1 pt-3 mb-3 mt-3">
+          Your Games
         </h2>
 
       </div>
@@ -20,34 +21,48 @@ return (
 
         <div class="col-sm-6 col-md-4">
           <div class="card border-white">
-
             <div class="card-body">
-              <img class="bat" src={battle} alt="battlefield"></img>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="card border-white">
-
-            <div class="card-body">
-              <img class="bat" src={halo} alt="halo"></img>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="card border-white">
-            
-            <div class="card-body">
-            <img class="bat" src={league} alt="leagueOfLegends"></img>
+              <img class="border image-fluid" src={battle} alt="battlefield"></img>
             </div>
           </div>
         </div>
 
         <div class="col-sm-6 col-md-4">
           <div class="card border-white">
-            
             <div class="card-body">
-            <img class="bat" src={cod} alt="callfoduty"></img>
+              <img class="border image-flui" src={halo} alt="halo"></img>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="card border-white">
+            <div class="card-body">
+            <img class="border image-fluid" src={league} alt="leagueOfLegends"></img>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="card border-white">
+            <div class="card-body">
+            <img class="border image-fluid" src={cod} alt="callfoduty"></img>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="card border-white">
+            <div class="card-body">
+            <img class="border image-fluid" src={forza} alt="callfoduty"></img>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="card border-white">
+            <div class="card-body">
+            <img class="border image-fluid" src={stardew} alt="callfoduty"></img>
             </div>
           </div>
         </div>
