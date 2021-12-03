c007e26047eeeea66836b261b37066aee110912b
Leah Davis <ileahd96@gmail.com>
2021-12-03 13:06:33 -0500

    Added schedule section and created working sticky sidebar

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,6 +4,10 @@ import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import "../../src/styles.css";
 import profile from "../assets/images/profile/profile.jpg";
+import stardew from "../assets/images/profile/stardew.jpg";
+import mario from "../assets/images/profile/mariokart.jpg";
+import itTakes2 from "../assets/images/profile/ittakes2.jpg";
+import ac from "../assets/images/profile/ac.jpg";
 
 import Auth from '../utils/auth';
 import { Link } from 'react-router-dom';
@@ -35,60 +39,88 @@ const Profile = () => {
   // }
 
   return (
-    <div className="container mt-4">
-      <div className="flex-row mb-3">
-        <img className="image-fluid profile-img" src={profile} alt=""></img>
-        <div className="col-5 p-3">
-            <h2 className="mb-3 d-flex justify-content-start">
-            <svg className="online-icon mr-2" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#2aeb3d" class="bi bi-circle-fill" viewBox="0 0 16 16">
-              <circle cx="8" cy="8" r="8"/></svg>
-              {userParam ? `${user.username}'s` : "User"}'s Profile 
-            </h2>
-            <h6 className="ml-2"><b>Platforms:</b> PC, Switch, Playstation</h6>
-            <h6 className="mt-2 ml-2"><b>Last Online:</b> Now</h6>
-            <p className="mt-2 ml-2 mt-4">Hey, I'm Sarah. I play all types of games but particularly enjoy multiplayer on PC, Switch, and Playstation. I'm into more casual games so if you're into Stardew Valley, Animal Crossing, or Mario Kart, let's play together! </p>
+    <div className="container">
+    <div className="row py-3">
+        <div className="col-3 order-2" id="sticky-sidebar">
+            <div className="sticky-top">
+                <div className="flex-column">
+                <div className="friends-search-bar">
+                    <Link className="navItem" to="/search">
+                      <form className="form-inline input-group" id="searchFriend">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Find a friend" aria-label="Search" id="searchInput"></input>
+                        <button className="friends-btn my-5 my-sm-0" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                              </svg>
+                        </button>
+                      </form>
+                    </Link>
+                </div>
+                <div className="sidebar">
+                    <h4><b>Games</b> <span className="red-text">86</span></h4>
+                    <h4 className="mt-2"><b>Groups</b> <span className="red-text">3</span></h4>
+                    <h4 className="mt-2"><b>Friends</b> <span className="red-text">18</span></h4>
+                    <Link className="profile-sidebar-link" to="/guardian855">
+                      <p className="mt-1">guardian855</p>
+                    </Link>
+                    <Link className="profile-sidebar-link" to="/pledias25">
+                      <p className="mt-1">pledias25</p>
+                    </Link>
+                      <h4 className="mt-3"><b>Quick Links</b></h4>
+                    <Link className="profile-sidebar-link" to="/wishlist">
+                      <p className="mt-1">Wish List</p>
+                    </Link>
+                    <Link className="profile-sidebar-link" to="/Library">
+                      <p className="">Library</p>
+                    </Link>
+                </div>
+                </div>
+            </div>
         </div>
-        <div className="col-3 p-3 ml-5">
-          <div className="friends-search-bar">
-            <Link className="navItem" to="/search">
-                    <form className="form-inline input-group" id="searchFriend">
-                      <input className="form-control mr-sm-2" type="search" placeholder="Find a friend" aria-label="Search" id="searchInput"></input>
-                      <button className="friends-btn my-5 my-sm-0" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                            </svg>
-                      </button>
-                    </form>
-            </Link>
+        <div className="col" id="main">
+          <div className="row">
+            <img className="img-fluid profile-img col-5" src={profile} alt=""></img>
+            <div className="col-7">
+              <h2 className="mb-3 mt-4 d-flex justify-content-start">
+              <svg id="online-icon" className="mr-2" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#2aeb3d" class="bi bi-circle-fill" viewBox="0 0 16 16">
+                <circle cx="8" cy="8" r="8"/></svg>
+                {userParam ? `${user.username}'s` : "User"}'s Profile 
+              </h2>
+              <h6 className="ml-2"><b>Platforms:</b> PC, Switch, Playstation</h6>
+              <h6 className="mt-2 ml-2"><b>Last Online:</b> Now</h6>
+              <p className="mt-2 ml-2 mt-4">Hey, I'm Sarah. I play all types of games but particularly enjoy multiplayer on PC, Switch, and Playstation. I'm into more casual games so if you're into Stardew Valley, Animal Crossing, or Mario Kart, let's play together! </p>
+            </div>
           </div>
-          <div className="sidebar p-3">
-            <h4><b>Games</b> <span className="red-text">86</span></h4>
-            <h4 className="mt-2"><b>Friends</b> <span className="red-text">18</span></h4>
-              <Link className="profile-sidebar-link" to="/guardian855">
-                <p className="mt-1">guardian855</p>
-              </Link>
-              <Link className="profile-sidebar-link" to="/pledias25">
-                <p className="mt-1">pledias25</p>
-              </Link>
-            <h4 className="mt-3"><b>Quick Links</b></h4>
-            <Link className="profile-sidebar-link" to="/wishlist">
-              <p className="mt-1">Wish List</p>
-            </Link>
-            <Link className="profile-sidebar-link" to="/Library">
-              <p className="">Library</p>
-            </Link>
+          <div className="row mt-5 mb-4">
+            <h2>Upcoming Schedule</h2>
+            <div>
+              <div className="col-12 flex-row mt-2">
+                <div className="col-3">
+                    <img className="scheduled-game mt-3" src={stardew} alt="Pixel house in front of mountains"></img>
+                    <h6 className="mt-3"><b>Sat, Dec 11</b></h6>
+                    <p className="small"> 7:00pm - 9:00pm EST</p>
+                </div>
+                <div className="col-3 ml-4">
+                    <img className="scheduled-game mt-3" src={mario} alt="Pixel house in front of mountains"></img>
+                    <h6 className="mt-3"><b>Sun, Dec 12</b></h6>
+                    <p className="small"> 6:00pm - 10:00pm EST</p>
+                </div>
+                <div className="col-3 ml-4">
+                    <img className="scheduled-game mt-3" src={itTakes2} alt="Pixel house in front of mountains"></img>
+                    <h6 className="mt-3"><b>Wed, Dec 15</b></h6>
+                    <p className="small"> 9:00pm - 11:00pm EST</p>
+                </div>
+                <div className="col-3">
+                    <img className="scheduled-game mt-3" src={ac} alt="Pixel house in front of mountains"></img>
+                    <h6 className="mt-3"><b>Fri, Dec 17</b></h6>
+                    <p className="small"> 10:00pm - 1:00am EST</p>
+                </div>
+                
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="row">
-
-      </div>
-
-
-
-
-
-
     </div>
+  </div>
   );
 };
 
