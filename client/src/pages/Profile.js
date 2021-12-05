777c1907c90645fe93f0ba35a2fa4502cbf91f4b
AWSMProdigy <StraightAKyle@gmail.com>
2021-12-05 15:13:51 -0500

    changed function name

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -273,7 +273,7 @@ const Profile = () => {
                 <div className="navbar-collapse sidebar-medium" id="sidebarNav">
                   <div className="friends-search-bar">
                       <Link className="navItem" to="/search">
-                        <form className="form-inline input-group" id="searchFriend" onSubmit={handleFormSubmit}>
+                        <form className="form-inline input-group" id="searchFriend" onSubmit={handleFriendSubmit}>
                           <input className="form-control mr-sm-2" type="search" placeholder="Find a friend" aria-label="Search" id="searchInput"></input>
                           <button className="friends-btn my-5 my-sm-0" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
