6b4d7cb1b1bb8861d1c977f268e8cc6ee7fbab9c
Leah Davis <ileahd96@gmail.com>
2021-12-06 20:54:12 -0500

    Fixed library and returned to row formatting

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect }  from 'react';
 import "../../src/styles.css";
 import Select from 'react-select';
 
-// import battle from "../assets/images/libraryImages/battlefield.jpg"
-// import halo from "../assets/images/libraryImages/haloinfinite.jpg"
-// import league from "../assets/images/libraryImages/LoL.jpg"
-// import cod from "../assets/images/libraryImages/blackops.jpg"
-// import forza from "../assets/images/libraryImages/forza.jpg"
-// import stardew from "../assets/images/libraryImages/stardew.jpg"
+import battle from "../assets/images/libraryImages/battlefield.jpg"
+import halo from "../assets/images/libraryImages/haloinfinite.jpg"
+import league from "../assets/images/libraryImages/LoL.jpg"
+import cod from "../assets/images/libraryImages/blackops.jpg"
+import forza from "../assets/images/libraryImages/forza.jpg"
+import stardew from "../assets/images/libraryImages/stardew.jpg"
 import { useQuery } from '@apollo/client';
 import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
@@ -82,7 +82,8 @@ const Library = () => {
     console.log(data.me.games);
     const gameTitles = data.games.map(a =>({label: a.title, value: a.title}));
     return (
-      <div id="games-library" className="pb-3 bg-black px-3">
+      <main className="bg-black">
+        <div id="games-library" className="pb-3 px-3">
         <div className="mb-3 d-flex justify-content-between align-items-end">
             <h2 className="p-2 ml-2 mt-4 text-white">My Games</h2>
             <div className="flex-row pb-2">
@@ -91,85 +92,24 @@ const Library = () => {
             </div>
         </div>
 
+        <div className="flex-row">
         {data.me.games.map((game, index) => (
-          <div className="col-sm-6 col-md-4 col-lg-2">
+        <div className="col-sm-6 col-md-4 col-lg-2">
           <div className="p-3 game-library">
-            {game}
             <Link to={`/game/${game}`}>
             <img className="img-fluid pop-games" src={getPic(game)} alt={game}></img>
             </Link>
-            <button className="custom-btn game-btn mx-auto mt-3" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+            {/* <button className="custom-btn game-btn mx-auto mt-3" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
               <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
               </svg>
-            </button>
+            </button> */}
           </div>
+        </div>
+          ))}
+        </div>
       </div>
-        ))}
-          {/* <div className="row">
-            <div className="col-sm-6 col-md-4 col-lg-2">
-                <div className="p-3 game-library">
-                  <img className="img-fluid pop-games" src={battle} alt="battlefield"></img>
-                  <button className="custom-btn game-btn mx-auto mt-3" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                  </button>
-                </div>
-            </div>
-  
-            <div className="col-sm-6 col-md-4 col-lg-2">
-                <div className="p-3 game-library">
-                  <img className="img-fluid pop-games" src={halo} alt="Halo"></img>
-                  <button className="custom-btn game-btn mx-auto mt-3" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                  </button>
-                </div>
-            </div>
-  
-            <div className="col-sm-6 col-md-4 col-lg-2">
-                <div className="p-3 game-library">
-                  <img className="img-fluid pop-games" src={league} alt="League"></img>
-                  <button className="custom-btn game-btn mx-auto mt-3" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                  </button>
-                </div>
-            </div>
-  
-      
-            <div className="col-sm-6 col-md-4 col-lg-2">
-                <div className="p-3 game-library">
-                  <img className="img-fluid pop-games" src={cod} alt="Call of Duty"></img>
-                  <button className="custom-btn game-btn mx-auto mt-3" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                  </button>
-                </div>
-            </div>
-  
-  
-            <div className="col-sm-6 col-md-4 col-lg-2">
-                <div className="p-3 game-library">
-                  <img className="img-fluid pop-games" src={forza} alt="Forza"></img>
-                  <button className="custom-btn game-btn mx-auto mt-3" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                  </button>
-                </div>
-            </div>
-  
-  
-            <div className="col-sm-6 col-md-4 col-lg-2">
-                <div className="p-3 game-library">
-                  <img className="img-fluid pop-games" src={stardew} alt="Stardew Valley"></img>
-                  <button className="custom-btn game-btn mx-auto mt-3" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                  </button>
-                </div>
-            </div>
-          </div> */}
-      </div>
+      </main>
+
     );
   };
   }
