9ccc329f0f9cbc8fa9c02d3ed09cad388185c46c
AWSMProdigy <StraightAKyle@gmail.com>
2022-03-08 22:13:31 -0500

    Search feature working properly

--- client/src/pages/Browse.js
+++ client/src/pages/Browse.js
@@ -6,40 +6,72 @@ import { QUERY_LIBRARY } from '../utils/queries';
 import { ADD_GAME } from '../utils/mutations';
 
 const Browse = () => {
-    const { loading, data } = useQuery(QUERY_LIBRARY);
-    const [addUserGame] = useMutation(ADD_GAME);
-    const [selectedGame, setGame] = useState("");
+  const { loading, data } = useQuery(QUERY_LIBRARY);
+  const [addUserGame] = useMutation(ADD_GAME);
+  const [selectedGame, setGame] = useState("");
+  const [listedGames, setListed] = useState([]);
 
-    const addGame = async () => {
-        console.log(selectedGame);
-        try{
-          const { data } = await addUserGame({
-            variables: {
-              title: selectedGame
-            }
-          })
-          console.log(selectedGame);
-        }
-        catch(err){
-          console.error(err);
-        }
-    }
-
-    const handleChange = (event) => {
-        if(event){
-          setGame(event.value);
-        }
-      }
-
+  useEffect(() => {
     if(loading){
-        return(<h1>Loading...</h1>)
-    }
-    else {
-      {data.games.map((game) => (
-        <h1>{game}</h1>
-      ))}
-    }
-    
 
+    }
+    else{
+      setListed(data.games)
+    }
+  }, [loading]);
+
+  const addGame = async () => {
+      console.log(selectedGame);
+      try{
+        const { data } = await addUserGame({
+          variables: {
+            title: selectedGame
+          }
+        })
+        console.log(selectedGame);
+      }
+      catch(err){
+        console.error(err);
+      }
+  }
+
+  const handleChange = (event) => {
+      if(event){
+        setGame(event.value);
+      }
+    }
+
+  const handleSearchChange = (event) => {
+    console.log(event.target.value);
+    setListed(data.games.filter(game => {
+      return game.title.includes(event.target.value);
+    }))
+  }
+
+  if(loading){
+      return(<h1>Loading...</h1>)
+  }
+  else {
+    return(
+      <main>
+      <div className='filters'>
+        <div className='d-flex w-100 bg-black'>
+          <h2>Action</h2>
+          <h2>Racing</h2>
+          <h2>Open-world</h2>
+          <h2>Strategy</h2>
+          <h2>RPG</h2>
+        </div>
+        <input className="form-control mr-sm-2" type="search" placeholder="Search game" aria-p="Search" id="searchInput" onInput={(e) => handleSearchChange(e)}></input>
+      </div>
+      {listedGames.map((game, index) => (
+        <h1>{game.title}</h1>
+      ))}
+      </main>
+    )
+  }
+    
 }
+
+export default Browse;
 
