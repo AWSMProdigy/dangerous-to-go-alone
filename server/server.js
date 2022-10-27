1ec67599f253d6dab92b4ace76a4ef8550e8dac6
AWSMProdigy <StraightAKyle@gmail.com>
2022-10-27 13:16:31 -0400

    Website now being serverd solely by express

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.static('public'));
+
 const mongodb = require('mongodb');
 const fs = require('fs');
 
@@ -48,12 +48,13 @@ server.applyMiddleware({ app,
 });
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "client/build", "index.html"));
 });
 
 app.get("/:filename", function(req, res){ 
