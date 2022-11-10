fb8ac3305e8bfe9c1461a495beec657cfa098146
AWSMProdigy <StraightAKyle@gmail.com>
2022-11-10 16:11:49 -0500

    Popups now taking info properly, styling popups now

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -5,6 +5,18 @@ function Popup(props){
     return ( props.state.trigger) ? (
         <div className='popup'>
             <div className='popupInner'>
+                <div className='popupHeader'>
+                    <h1>{props.state.title}<span> by {props.state.creator}</span></h1>
+                </div>
+                <div className='popupBody'>
+                    <div className='popupInfo'>
+                        <h2>Player Count: {props.state.players.length}/{props.state.capacity}</h2>
+                        <h2>Playstyle: {props.state.playstyle}</h2>
+                    </div>
+                    <div className='popupPlayers'>
+                        <h2>{props.state.players}</h2>
+                    </div>
+                </div>
                 <button className='closeBtn' onClick={() => props.setPopup({trigger: false})}>Close</button>
                 {props.children}
             </div>
